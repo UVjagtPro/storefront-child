c98b38ddf14697915815af4466e3526e42ffa536
UVjagtPro <36680702+UVjagtPro@users.noreply.github.com>
2018-05-02 23:32:27 +0200

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+		/*
+	     * Template Name: Arkiv
+	     */
+
+/**
+
+ * The template for displaying archive pages.
+ *
+ * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package storefront
+ */
+
+	get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<div class="woocommerce columns-3">
+				<ul class="products columns-3">
+
+					<?php 
+
+			        $query_params = array(
+			                'post_type' => 'post',
+			                'posts_per_page' => 3 // Value "-1" displays all products in feed
+			        );
+
+			        $wp_query = new WP_Query( $query_params);        
+
+			        if( $wp_query->have_posts() ) :
+
+						while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+
+							<li class="product post-item">
+								<span class="post-image">
+									<a href="<?php the_permalink(); ?>">
+										<?php 
+											if ( has_post_thumbnail()) 
+											{
+												the_post_thumbnail();
+											}
+										?>
+									</a>
+								</span>
+								<h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+								<span class="post-category"><?php the_category(', ');?></span>
+							</li>
+
+						<?php endwhile; ?>
+
+					<?php endif; ?>
+
+				</ul>
+
+				<nav>
+					<?php
+					
+					//global $wp_query; // you can remove this line if everything works for you
+					 
+					// don't display the button if there are not enough posts
+					if (  $wp_query->max_num_pages > 1 )
+						echo '
+							<div class="wordpress_wrapper">
+								<div class="wordpress_loadmore">More posts</div>
+							</div>'; // you can use <a> as well
+					?>
+				</nav>
+
+				
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+/*do_action( 'storefront_sidebar' );*/
+get_footer();
---
+++ woocommerce-google-merchant-feed.php
@@ -0,0 +1,73 @@
+<?php
+        /**
+         * Template Name: WooCommerce Google Merchant feed
+         */
+
+        $params = array(
+                'posts_per_page' => -1, // Value "-1" displays all products in feed
+                'post_type' => 'product'
+        );
+
+        $wc_query = new WP_Query($params);
+
+        header('Content-Type: '.feed_content_type('rss-http').'; charset='.get_option('blog_charset'), true);
+        echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
+
+?>
+
+<rss version="2.0"
+        xmlns:content="http://purl.org/rss/1.0/modules/content/"
+        xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        xmlns:atom="http://www.w3.org/2005/Atom"
+        xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+        xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+        xmlns:g="http://base.google.com/ns/1.0">
+        <?php do_action('rss2_ns'); ?>>
+        
+        <channel>
+                <title><?php bloginfo_rss('name'); ?> - Google Merchant Feed</title>
+                <atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
+                <link><?php bloginfo_rss('url') ?></link>
+                <description><?php bloginfo_rss('description') ?></description>
+                <lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false); ?></lastBuildDate>
+                <sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
+                <sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
+                
+                <?php do_action('rss2_head'); ?>
+
+                <?php while ($wc_query->have_posts()) : $wc_query->the_post(); ?>  
+ 
+                        <?php $regularPrice = get_post_meta( get_the_ID(), '_regular_price', true); ?>
+                        <?php $salePrice = get_post_meta( get_the_ID(), '_sale_price', true); ?>
+                        <?php $stockStatus = get_post_meta( get_the_ID(), '_stock_status', true); ?>
+                        <?php $stockQuantity = $product->get_stock_quantity(); ?>
+                        <?php $currency = get_woocommerce_currency_symbol(); ?>
+                        <?php $brand = get_post_meta( get_the_ID(), '_brand', true ); ?>
+                        <?php $condition = get_post_meta( get_the_ID(), '_condition', true ); ?>
+                        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+
+                        <item>
+                                <g:id><?php echo $product->get_sku(); ?></g:id>
+                                <title><?php echo $product->get_name(); ?></title>
+                                <g:description><?php echo $product->get_short_description(); ?></g:description>
+                                <link><?php the_permalink_rss(); ?></link>
+                                <g:image_link><?php echo $image[0]; ?></g:image_link>
+                                <g:availability> <?php if ($stockQuantity >= 1) 
+                                {
+                                        echo 'in stock';
+                                } else {
+                                        echo 'out of stock';
+                                } 
+                                ?></g:availability>
+                                <g:price><?php echo $regularPrice ?> <?php echo $currency ?></g:price>
+                                <g:brand><?php echo $brand ?></g:brand>
+                                <g:condition><?php echo $condition ?></g:condition>
+
+                                <?php rss_enclosure(); ?>
+                                <?php do_action('rss2_item'); ?>
+                        </item>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+        </channel>
+</rss>
